76d8a692a043d920126947e3052a2db089dbb9a6
JoaquinDecroly <joaquinottonello@gmail.com>
2025-10-04 19:45:59 +0200

    actividad 12 terminada

--- Actividad 2/src/Main.java
+++ Actividad 2/src/Main.java
@@ -125,61 +125,155 @@ public class Main {
 //        }
 
 
-        //ejercicio 9
-        Scanner sc3 = new Scanner(System.in);
+//        //ejercicio 9
+//        Scanner sc3 = new Scanner(System.in);
+//        
+//        int hora = 0;
+//        int min = 0;
+//        int seg = 0;
+//
+//        boolean hora1 = false;
+//        boolean min1 = false;
+//        boolean seg1 = false;
+//        do {
+//            System.out.println("Escriba una hora (24h max): ");// 23 horas, 59 minutos, 59 segundos = 00:00:00
+//            hora = sc3.nextInt();
+//            
+//            if (hora >= 0 && hora <= 23) {
+//                hora1 = true;
+//                
+//                System.out.println("Escriba un minuto (0-59): "); //59 minutos y 59 seg = 1 hora mas
+//                min = sc3.nextInt();
+//                
+//                if (min >= 0 && min <= 59) {
+//                    min1 = true;
+//                    
+//                    System.out.println("Escriba un segundo (0-59): ");//59 segundos = 1 minuto mas
+//                    seg = sc3.nextInt();
+//
+//                    if(seg >= 0 && seg <= 59){
+//                        seg1 = true;
+//                    }else{
+//                        seg1 = false;
+//                    }
+//                }else{
+//                    min1 = false;
+//                }
+//            }else{
+//                hora1 = false;
+//            }
+//        } while (!hora1 || !min1 || !seg1);
+//
+//        seg++;//sumar 1 segundo
+//
+//        if (hora == 24) { //si 
+//            hora = 0;
+//            min = 0;
+//            seg = 0;
+//        }else if(min == 60){//si minuto = 60, volver a 0 y sumar 1 a hora
+//            min = 0;
+//            hora++;
+//        }else if(seg == 60){ //si segundo = 60, volver a 0 y sumar 1 a minuto
+//            seg = 0;
+//            min++;
+//        }
+//        
+//        System.out.printf("%02d:%02d:%02d", hora, min, seg); // mostrar hora formateada con 2 digitos(%02d) + ":"
+//
+//        //ejercicio 10
+//        Scanner sc4 = new Scanner(System.in);
+//        int[] nums = new int[10];
+//        
+//        boolean hayNegativo = false;
+//        
+//        for(int i = 0; i < nums.length; i++){ //bucle leer numero con longitud array nums(10)
+//            System.out.println("Inserte 10 numeros: ");
+//                nums[i] = sc4.nextInt();
+//        }
+//        
+//        for(int num : nums){//recorrer array
+//            if(num < 0){
+//                hayNegativo = true;
+//                break;
+//            }
+//        }
+//        
+//        if(hayNegativo){
+//            System.out.println("Hay al menos un numero negativo");
+//        }else{
+//            System.out.println("No hay numeros negativos");
+//        }
         
-        int hora = 0;
-        int min = 0;
-        int seg = 0;
+//        //ejercicio 11
+//        Scanner sc5 = new Scanner(System.in);
+//        int[] nums1 = new int[10];
+//
+//        int contP = 0;
+//        int contN = 0;
+//
+//        for(int i = 0; i < nums1.length; i++){ //bucle leer numero con longitud array nums(10)
+//            System.out.println("Inserte 10 numeros: ");
+//            nums1[i] = sc5.nextInt();
+//        }
+//
+//        for(int num : nums1){//recorrer array
+//            if(num < 0){
+//                contN++;
+//            }else if(num > (-1)){
+//                contP++;
+//            }
+//        }
+//        System.out.println("Hay un total de " + contP + " numeros positivos");
+//        System.out.println("Hay un total de " + contN + " numeros negativos");
 
-        boolean hora1 = false;
-        boolean min1 = false;
-        boolean seg1 = false;
+        //ejercicio 12
+        Scanner sc6 = new Scanner(System.in);
+        int[] nums2 = new int[50];
+        
+        
+
+        boolean hayNegativo1 = false;
+        int contP1 = 0;//contador numeros positivos
+        int contN1 = 0;//contador numeros negativos
+
+        int cont = 0; //contador total numeros
+        
         do {
-            System.out.println("Escriba una hora (24h max): ");// 23 horas, 59 minutos, 59 segundos = 00:00:00
-            hora = sc3.nextInt();
-            
-            if (hora >= 0 && hora <= 23) {
-                hora1 = true;
-                
-                System.out.println("Escriba un minuto (0-59): "); //59 minutos y 59 seg = 1 hora mas
-                min = sc3.nextInt();
-                
-                if (min >= 0 && min <= 59) {
-                    min1 = true;
-                    
-                    System.out.println("Escriba un segundo (0-59): ");//59 segundos = 1 minuto mas
-                    seg = sc3.nextInt();
+            System.out.println("Inserte numeros(0 para finalizar): ");
+            int numero = sc6.nextInt();
 
-                    if(seg >= 0 && seg <= 59){
-                        seg1 = true;
-                    }else{
-                        seg1 = false;
-                    }
-                }else{
-                    min1 = false;
-                }
-            }else{
-                hora1 = false;
+            if (numero != 0) {
+                nums2[cont] = numero;
+                cont++;
+            } else {
+                break; // Salir del bucle cuando se introduce 0
             }
-        } while (!hora1 || !min1 || !seg1);
 
-        seg++;//sumar 1 segundo
+        } while (cont < 50);
+        
+        
 
-        if (hora == 24) { //si 
-            hora = 0;
-            min = 0;
-            seg = 0;
-        }else if(min == 60){//si minuto = 60, volver a 0 y sumar 1 a hora
-            min = 0;
-            hora++;
-        }else if(seg == 60){ //si segundo = 60, volver a 0 y sumar 1 a minuto
-            seg = 0;
-            min++;
+        for(int i = 0;i < cont; i++){//recorrer array
+           if(nums2[i] < 0){
+                hayNegativo1 = true;
+                contN1++;
+            }else if(nums2[i] > 0){
+                contP1++;
+            }
+        }
+
+        System.out.println("El array tiene un total de: " + cont + " numeros");
+
+        if(hayNegativo1){
+            System.out.println("Hay al menos un numero negativo");
+        }else{
+            System.out.println("No hay numeros negativos");
         }
         
-        System.out.printf("%02d:%02d:%02d", hora, min, seg); // mostrar hora formateada con 2 digitos(%02d) + ":"
-
-
+        System.out.println("Hay un total de " + contP1 + " numeros positivos (el 0 no es contado)");
+        System.out.println("Hay un total de " + contN1 + " numeros negativos");
+        
+        
+        
     } //main
 } //class
