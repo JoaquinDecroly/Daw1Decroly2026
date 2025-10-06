e442b719eb71750b3ad2161d1412741bcfacf1ef
Joaquin Ottonello <joaquinottonello@protonmail.com>
2025-10-06 09:57:24 +0200

    actividad 3.2 hecho falta 3.1

---
+++ Actividad 3/src/Main.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+         //ejercicio 1
+        Scanner sc1 = new Scanner(System.in);
+
+
+
+        //ejercicio 2
+        Scanner sc2 = new Scanner(System.in);
+
+        String option = "";
+
+        do{
+            System.out.println("1. Sumar");
+            System.out.println("2. Restar");
+            System.out.println("3. Multiplicar");
+            System.out.println("4. Dividir");
+            System.out.println("5. Salir");
+            option = sc2.nextLine();
+
+            switch(option){
+                case "1":
+                    System.out.println("Numero 1: ");
+                    double num1 = sc2.nextDouble();
+                    System.out.println("Numero 2: ");
+                    double num2 = sc2.nextDouble();
+                    double suma = num1 + num2;
+                    System.out.println("La suma es: " + suma);
+                break;
+                case "2":
+                    System.out.println("Numero 1: ");
+                    double num21 = sc2.nextDouble();
+                    System.out.println("Numero 2: ");
+                    double num22 = sc2.nextDouble();
+
+                    double resta = num21 + num22;
+                    System.out.println("La resta es: " + resta);
+                break;
+                case "3":
+                    System.out.println("Numero 1: ");
+                    double num31 = sc2.nextDouble();
+                    System.out.println("Numero 2: ");
+                    double num32 = sc2.nextDouble();
+
+                    double producto = num31 * num32;
+                    System.out.println("El producto es: " + producto);
+                break;
+                case "4":
+                    System.out.println("Numero 1: ");
+                    double num41 = sc2.nextDouble();
+                    System.out.println("Numero 2: ");
+                    double num42 = sc2.nextDouble();
+                break;
+                case "5":
+                    break;
+                default:
+                    System.out.println("Escoja una opcion correcta");
+                break;
+            }
+
+        }while(!option.equals("5"));
+    }
+}
