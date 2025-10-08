60e5d0f908b619af94637ddbcfd6be9ecafff41e
Joaquin Ottonello <joaquinottonello@protonmail.com>
2025-10-08 09:13:28 +0200

    no message

--- Actividad 2/src/Main.java
+++ Actividad 2/src/Main.java
@@ -1,8 +1,6 @@
 import java.util.Scanner;
-//import java.time.temporal;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
 
