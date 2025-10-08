9152fdb620126da67ac9e78ccb2894da2ad44839
Joaquin Ottonello <joaquinottonello@protonmail.com>
2025-10-08 09:02:15 +0200

    no message

--- Actividad 4/src/Main.java
+++ Actividad 4/src/Main.java
@@ -1,21 +1,25 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-    //ejercicio 1
+        //ejercicio 1
         Scanner sc1 = new Scanner(System.in);
 
         int[] nums1 = new int[10];
 
 
+
         System.out.println("Inserte 10 numeros");
-        
-            for(int i = 1;i < nums1.length;i++){
-                System.out.println("Numero " + i +"/10");
-                nums1[i] = sc1.nextInt();
-            }
 
-
+        int i;
+        for (i = 0; i < nums1.length; i++) {
+            System.out.println("Numero " + i+ "/9");
+            nums1[i] = sc1.nextInt();
+        }
+//        for(num; nums1){
+//            System.out.println(num);
+//        }
 
 
     }
