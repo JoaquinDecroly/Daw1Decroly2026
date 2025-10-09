b2b12537c24a5b0a67d775308e5e331b94ad7081
JoaquinDecroly <joaquinottonello@gmail.com>
2025-10-09 19:11:09 +0200

    actividad 4: 9 hecho

--- Actividad 4/src/Main.java
+++ Actividad 4/src/Main.java
@@ -3,25 +3,171 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //ejercicio 1
-        Scanner sc1 = new Scanner(System.in);
+//        //ejercicio 1
+//        Scanner sc1 = new Scanner(System.in);
+//
+//        int[] nums1 = new int[10];
+//        
+//        System.out.println("Inserte 10 numeros");
+//
+//        int i;
+//        for (i = 0; i < nums1.length; i++) {
+//            System.out.println("Numero " + i + "/9");
+//            nums1[i] = sc1.nextInt();
+//        }
+//
+//
+//        System.out.println("\n Los numeros insertados son: \n");
+//        for(int num : nums1){
+//            System.out.println(num);
+//        }
 
-        int[] nums1 = new int[10];
+
+//        //ejercicio 2
+//        Scanner sc2 = new Scanner(System.in);
+//
+//        int[] nums2 = new int[10];
+//        int suma = 0;
+//        
+//        System.out.println("Inserte 10 numeros");
+//
+//        int i;
+//        for (i = 0; i < nums2.length; i++) {
+//            System.out.println("Numero " + i + "/9");
+//            nums2[i] = sc2.nextInt();
+//            
+//            suma += nums2[i];
+//        }
+//        System.out.println(i);
+
+//        //ejercicio 3
+//        Scanner sc3 = new Scanner(System.in);
+//
+//        double[] nums1 = new double[10];
+//
+//        System.out.println("Inserte 10 numeros");
+//
+//        int i;
+//        for (i = 0; i < nums1.length; i++) {
+//            System.out.println("Numero " + i + "/9");
+//            nums1[i] = sc3.nextInt();
+//        }
+//        double max = Arrays.stream(nums1).max().getAsDouble();
+////            double max = Arrays.stream(nums1).max().orElse(0.0);
+//        double min = Arrays.stream(nums1).min().getAsDouble();
+////            double min = Arrays.stream(nums1).min().orElse(0.0);
+//        
+//        System.out.println("Maximo: " + max + "\n Minimo: " + min);
+
+//        //ejercicio 4
+//        Scanner sc4 = new Scanner(System.in);
+//
+//        int[] nums3 = new int[20];
+//        int sumaP = 0;
+//        int sumaN = 0;
+//        
+//        
+//        System.out.println("Inserte 10 numeros");
+//
+//        int i;
+//        for (i = 0; i < nums3.length; i++) {
+//            System.out.println("Numero " + i + "/19");
+//            nums3[i] = sc4.nextInt();
+//            
+//            if(nums3[i] > -1){
+//                sumaP += nums3[i];
+//            }else{
+//                sumaN += nums3[i];
+//            }
+//        }
+//        System.out.println("Suma Numeros \n Positivos " + sumaP + " Negativos " + sumaN);
+
+//        //ejercicio 5
+//        Scanner sc5 = new Scanner(System.in);
+//
+//        double[] nums2 = new double[20];
+//        double suma = 0;
+//
+//
+//        System.out.println("Inserte 10 numeros");
+//
+//        int i;
+//        for (i = 0; i < nums2.length; i++) {
+//            System.out.println("Numero " + i + "/19");
+//            nums2[i] = sc5.nextInt();
+//
+//            suma += nums2[i];
+//            double media = suma / nums2.length;
+//        }
+//      
+//        System.out.println("Media: " + media);
+
+//        //ejercicio 6
+//        Scanner sc6 = new Scanner(System.in);
+//        
+//        System.out.println("Ingrese valor N (tamano array) ");
+//        int N = sc6.nextInt();
+//            int[] array = new int[N];
+//        System.out.println("Ingrese valor M ");
+//        int M = sc6.nextInt();
+//            Arrays.fill(array, M);
+//            
+//            for(int num : array){
+//                System.out.println("\n" + num);
+//            }         
+
+//        //ejercicio 7
+//        Scanner sc7 = new Scanner(System.in);
+//        System.out.println("P ");
+//        int P = sc7.nextInt();
+//        System.out.println("Q ");
+//        int Q = sc7.nextInt();
+//        
+//        int[] array1 = new int[]{P,Q};
+//        for(int num1 : array1){
+//                System.out.println("\n" + num1);
+//            }    
+
+//        //ejercicio 8
+//        Scanner sc8 = new Scanner(System.in);
+//        double[] array2 = new double[100];
+//
+//        System.out.println("Valor R: ");
+//        double R = sc8.nextDouble();
+//
+//        for(int i = 0;i < array2.length;i++){
+//            array2[i] = Math.random();
+//
+//            if(array2[i] >= R){
+//                System.out.println(array2[i] + " es superior a " + R);
+//            }
+//        }
+
+//        //ejercicio 9
+//        Scanner sc9 = new Scanner(System.in);
+//        int[] array3 = new int[100];
+//        
+//        System.out.println("Introduce N ");
+//        int N = sc9.nextInt();
+//        for(int i = 0;i < array3.length;i++){
+//            array3[i] = (int)(1 + Math.random() * 10);
+//            
+//            if(array3[i] == N){
+//                System.out.println(i);
+//            }
+//        }
+      
+
+            
+                
+            
         
-        System.out.println("Inserte 10 numeros");
-
-        int i;
-        for (i = 0; i < nums1.length; i++) {
-            System.out.println("Numero " + i + "/10");
-            nums1[i] = sc1.nextInt();
-        }
 
 
-        System.out.println("\n Los numeros insertados son: \n");
-        for(int num : nums1){
-            System.out.println(num);
-        }
+
 
 
     }
+    
+    
 }
