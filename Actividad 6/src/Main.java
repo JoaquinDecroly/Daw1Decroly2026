a7af4f1120b4b44afee6049e29b2e0082b6bdded
Joaquin Ottonello <joaquinottonello@protonmail.com>
2025-10-13 11:37:44 +0200

    Actividad 6, ejercicio 4 en desarrollo

--- Actividad 6/src/Main.java
+++ Actividad 6/src/Main.java
@@ -38,8 +38,54 @@ public class Main {
 
         //ejercicio 3
         Scanner sc = new Scanner(System.in);
-        System.out.println("Tamano N del array ");
+
+        int menores = 0;
+        int mayores = 0;
+        int ceros = 0;
+
+
+        System.out.println("Filas N ");
         int N = sc.nextInt();
-        System.out.println("Tamano M del array ");
+        System.out.println("Columnas M ");
+        int M = sc.nextInt();
+
+            int[][] matriz3 = new int[N][M];
+
+            for(int i = 0;i < matriz3.length;i++){
+                for(int j = 0;j < matriz3[i].length;j++){
+                    System.out.println("Valor [" + i +"][" + j + "]");
+                    matriz3[i][j] = sc.nextInt();
+                }
+            }
+
+
+        for(int i = 0;i < matriz3.length;i++){
+            for(int j = 0;j < matriz3[i].length;j++){
+                if(matriz3[i][j] < 0){
+                    menores++;
+                }
+                if(matriz3[i][j] > 0){
+                    mayores++;
+                }
+                if(matriz3[i][j] == 0){
+                    ceros++;
+                }
+                System.out.print(matriz3[i][j]);
+            }
+        }
+
+        System.out.println("Hay " + mayores + " numeros mayores a 0");
+        System.out.println("Hay " + menores + " numeros menores a 0");
+        System.out.println("Hay " + ceros + " numeros iguales a 0");
+
+        //ejercicio 4
+        Scanner sc1 = new Scanner(System.in);
+        String[][] array = new String[4][5];
+
+        for(int i = 1; i < array.length; i++){
+            for(int j = 0; j <= array[i].length; j++){
+                
+            }
+        }
     }
 }
