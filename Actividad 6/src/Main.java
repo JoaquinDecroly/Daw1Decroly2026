ccb23b5bbd1cde6f445e0099047563f22417120b
Joaquin Ottonello <joaquinottonello@protonmail.com>
2025-10-13 09:48:59 +0200

    actividad 6 comienzo

---
+++ Actividad 6/src/Main.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //ejercicio 1
+//        int[][] matriz = new int[5][5];
+//
+//
+//        for(int i = 0; i < 5;i++){
+//        for(int j = 0;j < 5;j++){
+//            matriz[i][j] =  (i * 5) + j + 1;
+//        }
+//        }
+//
+//        for(int i = 0; i < 5;i++){
+//            for(int j = 0;j < 5;j++){
+//                System.out.print(matriz[i][j] + " ");
+//                if((matriz[i][j] % 5)==0){
+//                    System.out.println();
+//                }
+//            }
+//        }
+
+        //ejercicio 2 (CORREGIR)
+//        int[][] matriz2 = new int[10][10];
+//
+//        for(int i = 0;i < matriz2.length;i++){
+//            for(int j = 0;i < matriz2[i].length;j++){
+//                matriz2[i][j] = (i * 10) + j + 1;
+//            }
+//        }
+//        for(int i = 0;i < matriz2.length;i++){
+//            for(int j = 1;i < matriz2[i].length;j++){
+//                System.out.print(matriz2[i][j]);
+//
+//            }
+//        }
+
+        //ejercicio 3
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Tamano N del array ");
+        int N = sc.nextInt();
+        System.out.println("Tamano M del array ");
+    }
+}
